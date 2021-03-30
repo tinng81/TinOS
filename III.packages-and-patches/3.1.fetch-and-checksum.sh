89e70286d929163b07edfec0d32c399b2b4287be
Tin Nguyen MBP <40393525+tinng81@users.noreply.github.com>
2021-03-31 01:44:15 +0700

    FIX relevant path to md5sum of all packages

--- III.packages-and-patches/3.1.fetch-and-checksum.sh
+++ III.packages-and-patches/3.1.fetch-and-checksum.sh
@@ -1,4 +1,4 @@
-# Usage: ./3.1.fetch-and-checksum.fs 
+# Usage: ./3.1.fetch-and-checksum.sh 
 #!/bin/bash
 
 . ../lfs-env
@@ -6,11 +6,14 @@
 # Create dir then manually copy wget-list, md5sum to $LFS/sources to fetch
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
-wget --input-file=../tools/wget-list --continue --directory-prefix=$LFS/sources
 
-# verify with checksum
-# md5sums of all packages' sum
+mv ../tools/wget-list $LFS/sources
+mv ../tools/md5sums $LFS/sources
+
 pushd $LFS/sources
-  md5sum -c ../tools/md5sums
+  wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+  # verify with checksum
+  # md5sums of all packages' sum
+  md5sum -c md5sums
 popd
 
