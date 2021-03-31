809258c81f51385be90d4f62cb3f7880c1ad929b
Tin Nguyen MBP <40393525+tinng81@users.noreply.github.com>
2021-03-31 20:42:09 +0700

    ADD tmp_tool scripts + FIX call CLI instead func

--- V.compiling-a-cross-toolchain/5.1.build-all-packages.sh
+++ V.compiling-a-cross-toolchain/5.1.build-all-packages.sh
@@ -15,9 +15,13 @@ function build_all_packages() {
       tar_dir=${tar_name%.tar.*}
       tar_script=${tar_dir%-*}
 
-      echo "$tar_dir"
-      echo "$tar_script"
-  
+      # Additional logic ensures correct (local) func invoked
+      case "$tar_script" in 
+      grep*|make*|tar*)
+        tar_script=_$tar_script
+        ;;
+      esac
+
       # Create and extract to folder
       # NOTE strip parent folder of all tarball
       # FIXME ensure none of the tarball missing a parent folder
@@ -29,9 +33,9 @@ function build_all_packages() {
       # TODO might consider strip version in folder/script name
       # NOTE time function reveals Standard Build Unit for each package
       pushd $tar_dir 
-        time {
-          "$tar_script"
-        }
+       time {
+         "$tar_script"
+       }
       popd
   
       # FIXME remove after development
@@ -154,11 +158,11 @@ function glibc() {
 
   # NOTE chained calling function with already extracted tarball
   # TODO Better practice
-  libstdc
+  _libstdc
 }
 
 # Libstd++ Pass 1
-function libstdc() {
+function _libstdc() {
   # NOTE compiled in GCC folder
   pushd $LFS/sources/gcc
       
@@ -177,6 +181,259 @@ function libstdc() {
     make
     make DESTDIR=$LFS install
   popd
+}
+
+function m4() {
+  sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+  echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+  
+  ./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
+
+  make
+  make DESTDIR=$LFS install
+}
+
+function ncurses() {
+  sed -i s/mawk// configure
+
+  mkdir -v build
+  pushd build
+    ../configure
+    make -C include
+    make -C progs tic
+  popd
+
+  ./configure --prefix=/usr                \
+              --host=$LFS_TGT              \
+              --build=$(./config.guess)    \
+              --mandir=/usr/share/man      \
+              --with-manpage-format=normal \
+              --with-shared                \
+              --without-debug              \
+              --without-ada                \
+              --without-normal             \
+              --enable-widec
+  
+  make
+  make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
+  echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
+
+  mv -v $LFS/usr/lib/libncursesw.so.6* $LFS/lib
+  ln -sfv ../../lib/$(readlink $LFS/usr/lib/libncursesw.so) $LFS/usr/lib/libncursesw.so
+}
+
+function bash() {
+  ./configure --prefix=/usr                   \
+              --build=$(support/config.guess) \
+              --host=$LFS_TGT                 \
+              --without-bash-malloc
+
+  make
+  make DESTDIR=$LFS install
+
+  mv $LFS/usr/bin/bash $LFS/bin/bash
+  ln -sv bash $LFS/bin/sh
+}
+
+function coreutils() {
+  ./configure --prefix=/usr                     \
+            --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess) \
+            --enable-install-program=hostname \
+            --enable-no-install-program=kill,uptime
+  
+  make
+  make DESTDIR=$LFS install
+
+  mv -v $LFS/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} $LFS/bin
+  mv -v $LFS/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm}        $LFS/bin
+  mv -v $LFS/usr/bin/{rmdir,stty,sync,true,uname}               $LFS/bin
+  mv -v $LFS/usr/bin/{head,nice,sleep,touch}                    $LFS/bin
+  mv -v $LFS/usr/bin/chroot                                     $LFS/usr/sbin
+  mkdir -pv $LFS/usr/share/man/man8
+  mv -v $LFS/usr/share/man/man1/chroot.1                        $LFS/usr/share/man/man8/chroot.8
+  sed -i 's/"1"/"8"/'                                           $LFS/usr/share/man/man8/chroot.8
+}
+
+function diffutils() {
+  ./configure --prefix=/usr --host=$LFS_TGT
+
+  make
+  make DESTDIR=$LFS install
+}
+
+function file() {
+  mkdir build
+  pushd build
+    ../configure --disable-bzlib      \
+                --disable-libseccomp \
+                --disable-xzlib      \
+                --disable-zlib
+    make
+  popd
+
+  ./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
+
+  make FILE_COMPILE=$(pwd)/build/src/file
+  make DESTDIR=$LFS install
+}
+
+function findutils() {
+  ./configure --prefix=/usr   \
+              --host=$LFS_TGT \
+              --build=$(build-aux/config.guess)
+
+  make
+  make DESTDIR=$LFS install
+
+  mv -v $LFS/usr/bin/find $LFS/bin
+  sed -i 's|find:=${BINDIR}|find:=/bin|' $LFS/usr/bin/updatedb
+}
+
+function gawk() {
+  sed -i 's/extras//' Makefile.in
+  ./configure --prefix=/usr   \
+              --host=$LFS_TGT \
+              --build=$(./config.guess)
+
+  make
+  make DESTDIR=$LFS install
+}
+
+# As oppposed to grep on host system
+function _grep() {
+  ./configure --prefix=/usr   \
+              --host=$LFS_TGT \
+              --bindir=/bin
+
+  make
+  make DESTDIR=$LFS install              
+}
+
+function gzip() {
+  ./configure --prefix=/usr --host=$LFS_TGT
+
+  make
+  make DESTDIR=$LFS install
+
+  mv -v $LFS/usr/bin/gzip $LFS/bin  
+}
+
+function _make() {
+  ./configure --prefix=/usr   \
+              --without-guile \
+              --host=$LFS_TGT \
+              --build=$(build-aux/config.guess)
+
+  make
+  make DESTDIR=$LFS install
+}
+
+function patch() {
+  ./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
+
+  make
+  make DESTDIR=$LFS install
+}
+
+function sed() {
+  ./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --bindir=/bin
+
+  make
+  make DESTDIR=$LFS install            
+}
+
+function _tar() {
+  ./configure --prefix=/usr                     \
+            --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess) \
+            --bindir=/bin
+
+  make
+  make DESTDIR=$LFS install
+}
+
+function xz() {
+  ./configure --prefix=/usr                     \
+            --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess) \
+            --disable-static                  \
+            --docdir=/usr/share/doc/xz-5.2.5
+
+  make
+  make DESTDIR=$LFS install
+
+  mv -v $LFS/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat}  $LFS/bin
+  mv -v $LFS/usr/lib/liblzma.so.*                       $LFS/lib
+  ln -svf ../../lib/$(readlink $LFS/usr/lib/liblzma.so) $LFS/usr/lib/liblzma.so
+
+  # NOTE chained calling function with already extracted tarball
+  # TODO Better practice
+  _binutils-p2
+  _gcc-p2
+}
+
+function _binutils-p2() {
+  # TODO refactor dirname to stripped down version, similar to gcc
+  pushd $LFS/sources/binutils-2.35
+  mkdir -v build-p2
+  cd       build-p2
+
+  ../configure                   \
+      --prefix=/usr              \
+      --build=$(../config.guess) \
+      --host=$LFS_TGT            \
+      --disable-nls              \
+      --enable-shared            \
+      --disable-werror           \
+      --enable-64-bit-bfd
+
+  make
+  make DESTDIR=$LFS install
+  install -vm755 libctf/.libs/libctf.so.0.0.0 $LFS/usr/lib
+}
+
+function _gcc-p2() {
+  pushd $LFS/sources/gcc
+  case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+  ;;
+  esac
+
+  mkdir -v build-p2
+  cd       build-p2
+
+  mkdir -pv $LFS_TGT/libgcc
+  ln -s ../../../libgcc/gthr-posix.h $LFS_TGT/libgcc/gthr-default.h
+
+  ../configure                                       \
+    --build=$(../config.guess)                     \
+    --host=$LFS_TGT                                \
+    --prefix=/usr                                  \
+    CC_FOR_TARGET=$LFS_TGT-gcc                     \
+    --with-build-sysroot=$LFS                      \
+    --enable-initfini-array                        \
+    --disable-nls                                  \
+    --disable-multilib                             \
+    --disable-decimal-float                        \
+    --disable-libatomic                            \
+    --disable-libgomp                              \
+    --disable-libquadmath                          \
+    --disable-libssp                               \
+    --disable-libvtv                               \
+    --disable-libstdcxx                            \
+    --enable-languages=c,c++
+  
+  make
+  make DESTDIR=$LFS install
+  ln -sv gcc $LFS/usr/bin/cc  
 }
 
 build_all_packages; exit
